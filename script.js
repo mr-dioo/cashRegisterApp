b4cab1920c6dcf72d00245f849265709ebdc7b9f
Hamid Davoudkhani <hdavoudkhani@uxbridgecollege.ac.uk>
2024-03-25 11:47:05 +0000

    defined variables

---
+++ script.js
@@ -0,0 +1,15 @@
+const purchaseBtn = document.getElementById("purchase-btn");
+const cashInput = document.getElementById("cash");
+const changeDue = document.getElementById("change-due");
+const drawerStatus = changeDue.querySelector("h2");
+const changeDueList = changeDue.querySelector("ul");
+
+let price = 0;
+let cid = [,];
+let cash = 0;
+
+
+const checkValidInput() {
+  
+}
+
