d2b2a4f11bbe3663736026df74551e4e6c9f35c0
Hamid Davoudkhani <hdavoudkhani@uxbridgecollege.ac.uk>
2024-03-25 16:14:00 +0000

    add updateStatus and get sum and status to the Drawer object

--- script.js
+++ script.js
@@ -5,12 +5,34 @@ const drawerStatus = changeDue.querySelector("h2");
 const changeDueList = changeDue.querySelector("ul");
 
 let price = 0;
-let cid = [,];
+let cid = [
+  ["PENNY", 1.01],
+  ["NICKEL", 2.05],
+  ["DIME", 3.1],
+  ["QUARTER", 4.25],
+  ["ONE", 90],
+  ["FIVE", 55],
+  ["TEN", 20],
+  ["TWENTY", 60],
+  ["ONE HUNDRED", 100]
+];
 let cash =0;
 
+class Drawer {
+  constructor(cid) {
+    this.cashInDrawer = cid;
+    this.status = "Open";
+  }
 
+  getSum() {
+    return parseFloat((cid).reduce((total , note) => total + note[1] , 0).toFixed(2))
+  }
+  // getStatus
+}
 
+const drawer = new Drawer(cid);
 
+console.log(drawer.getSum())
 
 purchaseBtn.addEventListener('click', (event) => {
   event.preventDefault();
@@ -19,5 +41,6 @@ purchaseBtn.addEventListener('click', (event) => {
     return; 
   }
 
-
 })
+
+
